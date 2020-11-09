3af694c7faaad876eb606c8d05aa11ac28cd6685
daryataratynova <darya.taratynova@nu.edu.kz>
2020-11-09 20:03:10 +0600

    Coursers, inverse of matrix with cach

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions should inverse the matrix using cash
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a "matrix" object with cash opportunity
 
 makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+  set<- fuction (y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmatrix<- function(matrix) m<<-mean
+  getmatrix<-function() m
+  list(set=set, get=get, 
+       setmatrix= setmatrix, getmatrix=getmatrix)
 
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve function calculates the inversion of special matrix,
+##in case if it was calculated the function take the inversion from 
+## cash
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       m<-x$getmatrix()
+       if(!is.null(m)){
+         message("getting cached data")
+         return(m)
+       }
+       data<-x$get()
+       m<solve(matrix, ...)
+       x$setmatrix(m)
+       m
 }
